763d69a6006af7463688ce3b8921045d016fa491
Tushit Shukla <43513188+shuklatushit@users.noreply.github.com>
2019-11-04 16:10:45 +0530

    Create HamiltonianandLagrangian.c

---
+++ HamiltonianandLagrangian.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+    int main()
+    {
+        int n,i,j;
+        scanf("%d",&n);
+        int a[n],c;
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        for(i=0;i<n-1;i++)
+        {   c=0;
+            for(j=i+1;j<n;j++)
+            {  
+                if(a[i]<a[j])
+                {  
+                    c=1;
+                    break;
+                }
+            }
+            if(c!=1)
+              printf("%d ",a[i]);
+        }
+        printf("%d ",a[n-1]);
+    }
